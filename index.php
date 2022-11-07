046b7643a8d6f3b3e58c50539555c3593bc3a76c
Lis-quino21 <106177117+Lis-quino21@users.noreply.github.com>
2022-11-07 19:21:01 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "Controladores/plantillaC.php";
+
+require_once "Controladores/usuariosC.php";
+require_once "Modelos/usuariosM.php";
+
+require_once "Controladores/carrerasC.php";
+require_once "Modelos/carrerasM.php";
+
+$plantilla = new Plantilla();
+$plantilla -> LlamarPlantilla();
